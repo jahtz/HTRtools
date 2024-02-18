8c4a97c00c5a2a3e1c53e2fa682c1b780dbde63b
jatzelberger <jatzelberger@gmail.com>
2024-02-18 17:47:26 +0100

    project structure

---
+++ cli.py
@@ -0,0 +1,29 @@
+import click
+
+#from modules.htr_parser.ods_to_csv import ods_to_csv
+#from modules.pagestats import regionstats_cli
+#from modules.pagesearch import pagesearch_cli
+
+
+@click.group()
+@click.help_option("--help", "-h")
+@click.version_option(
+    "0.1",
+    "-v", "--version",
+    prog_name="HTRtools",
+    message="%(prog)s v%(version)s - Developed at Centre for Philology and Digitality (ZPD), University of Würzburg"
+)
+def cli(**kwargs):
+    """
+    HTRtools main entry point.
+
+    Developed at Centre for Philology and Digitality (ZPD), University of Würzburg.
+    """
+
+
+# pagestats module
+#cli.add_command(regionstats_cli)
+
+# pagesearch_module
+#cli.add_command(pagesearch_cli)
+

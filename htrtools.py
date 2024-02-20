faded41e2484e96e28cfe21c89621f1e2e9017b2
jatzelberger <jatzelberger@gmail.com>
2024-02-20 19:33:46 +0100

    add ods2json script

--- htrtools.py
+++ htrtools.py
@@ -1,7 +1,7 @@
 import click
 
 from modules.pagexml import regionstats_cli, pagesearch_cli, pagefix_cli
-from modules.parser import csv2txt_cli
+from modules.parser import csv2txt_cli, ods2json_cli
 
 
 @click.group()
@@ -27,6 +27,7 @@ cli.add_command(pagefix_cli)
 
 # parser module
 cli.add_command(csv2txt_cli)
+cli.add_command(ods2json_cli)
 
 if __name__ == '__main__':
     cli()
--- modules/parser/__init__.py
+++ modules/parser/__init__.py
@@ -1,2 +1,2 @@
 from .csv2txt import csv2txt_cli
-
+from .ods2json import ods2json_cli

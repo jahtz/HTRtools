fbe60561fef23fb053b57fbbd8f5cc02d4b22f44
jatzelberger <jatzelberger@gmail.com>
2024-02-21 11:58:15 +0100

    add img2png script

--- htrtools.py
+++ htrtools.py
@@ -1,13 +1,13 @@
 import click
 
 from modules.pagexml import regionstats_cli, pagesearch_cli, pagefix_cli
-from modules.parser import csv2txt_cli, ods2json_cli
+from modules.parser import csv2txt_cli, ods2json_cli, img2png_cli, pdf2png_cli
 
 
 @click.group()
 @click.help_option("--help", "-h")
 @click.version_option(
-    "0.1",
+    "0.2",
     "-v", "--version",
     prog_name="HTRtools",
     message="%(prog)s v%(version)s - Developed at Centre for Philology and Digitality (ZPD), University of Würzburg"
@@ -28,6 +28,11 @@ cli.add_command(pagefix_cli)
 # parser module
 cli.add_command(csv2txt_cli)
 cli.add_command(ods2json_cli)
+cli.add_command(img2png_cli)
+cli.add_command(pdf2png_cli)
+
+# manipulation module
+
 
 if __name__ == '__main__':
     cli()
--- modules/parser/__init__.py
+++ modules/parser/__init__.py
@@ -1,2 +1,4 @@
 from .csv2txt import csv2txt_cli
 from .ods2json import ods2json_cli
+from .img2png import img2png_cli
+from .pdf2png import pdf2png_cli
